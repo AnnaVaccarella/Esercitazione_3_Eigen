eef1e76e6544a8f2bbc71d39fcf1c83a97813fa0
unknown <s283814@studenti.polito.it>
2025-03-28 17:02:34 +0100

    modifica funzioni (da void a Vector2d)

--- main.cpp
+++ main.cpp
@@ -4,17 +4,19 @@
 using namespace std;
 
 // PALU
-void solveWithPALU (const Matrix2d& A, const Vector2d& b) {
+Vector2d solveWithPALU (const Matrix2d& A, const Vector2d& b) {
 	PartialPivLU<Matrix2d> lu(A);
 	Vector2d x = lu.solve(b);
 	cout << "Soluzione con PALU: " << x.transpose() << endl;
+	return x;
 }
 
 // QR
-void solveWithQR (const Matrix2d& A, const Vector2d& b) {
+Vector2d solveWithQR (const Matrix2d& A, const Vector2d& b) {
 	HouseHolderQR<Matrix2d> qr(A);
 	Vector2d x = qr.solve(b);
 	cout << "Soluzione con QR: " << x.transpose() << endl;
+	return x;
 }
 
 // calcola errore relativo
@@ -24,3 +26,43 @@ void calculateError(const Vector2d& x, const Vector2d& expected) {
 }
 
 
+
+
+
+int main()
+{
+	//Sistemi lineari dati
+	Matrix2d A1, A2, A3;
+	Vector2d b1, b2, b3, expected;
+	//Inizializzazione del primo sistema
+	A1 << 5.547001962252291e-01, -3.770900990025203e-02, 
+	8.320502943378437e-01, -9.992887623566787e-01;
+	b1 << -5.169911863249772e-01, 1.672384680188350e-01;
+	
+	//Inizializzazione del secondo sistema
+	A2 << 5.547001962252291e-01, -5.540607316466765e-01,
+	8.320502943378437e-01, -8.324762492991313e-01;
+    b2 << -6.394645785530173e-04, 4.259549612877223e-04;
+	
+	//Inizializzazione del terzo sistema
+	A3 << 5.547001962252291e-01, -5.547001955851905e-01,
+	8.320502943378437e-01, -8.320502947645361e-01;
+	b3 << -6.400391328043042e-10, 4.266924591433963e-10;
+	
+	expected << -1.0, -1.0; //Soluzione attesa
+	
+	//Risoluzione del primo sistema
+	cout << "Sistema 1: " << endl;
+	x1_PALU = solveWithPALU(A1, b1);
+	calculateError(x1_PALU, expected);
+	x1_QR = solveWithQR(A1, b1);
+	calculateError(x1_QR, expected);
+
+
+	
+	
+	
+	
+	
+    return 0;
+}
