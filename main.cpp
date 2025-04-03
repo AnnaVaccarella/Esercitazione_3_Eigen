d560241b5348153e353c624ad4fa1b3705fd1da0
unknown <s283814@studenti.polito.it>
2025-04-03 17:47:38 +0200

    TestSolution

--- main.cpp
+++ main.cpp
@@ -1,3 +1,29 @@
+#include <iostream>
+#include "Eigen/Eigen" //libreria Eigen che serve per operare con matrici e vettori
+
+using namespace std;
+using namespace Eigen;
+
+/// \brief Test the real solution of system Ax = b
+/// \return the relative error for PALU solver
+/// \return the relative error for QR solver
+void TestSolution(const MatrixXd& A,
+				  const VectorXd& b,
+				  const VectorXd& solution,
+				  double& errRelPalu,
+				  double& errRelQR);
+
+/// \brief Solve linear system with PALU
+/// \return the solution
+VectorXd SolveSystemPALU(const MatrixXd& A,
+                         const VectorXd& b);
+
+/// \brief Solve linear system with PALU
+/// \return the solution
+VectorXd SolveSystemQR(const MatrixXd& A,
+                       const VectorXd& b); 
+
+
 int main()
 {
     return 0;
